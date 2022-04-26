99866146674973a9cf1ca9170ad54f17fa56abc6
Ram Gopal Singh <ram.singh@rakuten.com>
2022-04-27 00:11:55 +0530

    project set up

---
+++ src/main/java/com/inn/rst/App.java
@@ -0,0 +1,13 @@
+package com.inn.rst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
